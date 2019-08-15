09da46b356ddae5d55d1e049b7996c5a183bbcd3
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-15 23:49:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,4 +9,7 @@ def using_push (colors_in_the_rainbow, next_color)
     bouroughs_in_nyc.unshift("Staten Island")
   end
   
-  def using_pop ()
+  def using_pop (continents)
+    continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+    continents.pop
+  end
