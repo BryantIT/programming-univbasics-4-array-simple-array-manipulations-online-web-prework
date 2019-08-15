55f96cd0ddff5a736fb5b47e88843a52a32d83df
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-15 23:52:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,5 +11,5 @@ def using_push (colors_in_the_rainbow, next_color)
   
   def using_pop (continents)
     continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-    continents.pop()
+    continents.pop
   end
